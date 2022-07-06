8546335b4edf347e101e1cbf0bd3bfb51ca8bc6a
sameet <sameetmoosavi@yahoo.com>
2022-07-06 11:28:00 +0430

    font configs

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font :pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "CaskaydiaCove Nerd Font :pixelsize=16:antialias=true:autohint=true" };
+static char *font = "JetBrainsMono-Bold :pixelsize=16.8:antialias=true:autohint=true";
+static char *font2[] = { "CaskaydiaCove Nerd Font :pixelsize=16.8:antialias=true:autohint=true" };
 static int borderpx = 5;
 
 /*
@@ -26,7 +26,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.0;
+static float chscale = 0.9;
 
 /*
  * word delimiter string
